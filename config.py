559a308de3b3147bd7fa242ed9263dc8d1da323e
Tusharzzzzzhddz <tu.shar373736@gmail.com>
2024-11-16 12:49:34 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SHARP
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7702571525:AAEkRV-Ie-QlU2HlVmsOrx0GIBqwkIRdLEM")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Tushar112403")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6073688352")
+
+
+
+
+
+
+
+  
